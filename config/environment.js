dee6de47f4b6bbc762527e584c5602d88292302c
Daria Kafler <me@dariakafler.com>
2021-08-06 15:45:59 +0100

    deploying

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 export const port = process.env.PORT || 4000
 export const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/drinks-api'
-export const secret = process.env.SECRET || 'shhhh its a secret'
+export const secret = process.env.SECRET || 'shhhh its a secret!'
 
 
 
